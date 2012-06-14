16eac982b0c98c130571233ac83d93cfa08386d4
FrederiqueG <frederique.grd@gmail.com>
2012-06-14 14:07:01 -0400

    Initial Commit

---
+++ Jun14/Jun14AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun14AppDelegate.h
+//  Jun14
+//
+//  Created by Frederique Giraud on 6/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun14AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
